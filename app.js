fe5cdedecadb7bc5357a1e8a444970527a8c8f96
Evellyn <evellynlrl@hotmail.com>
2017-04-04 21:42:17 -0300

    Criando pasta do projeto com pergunta no terminal

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 'use strict'
+
+// const createProject = require('./files/create-project-folder')
--- files/create-project-folder.js
+++ files/create-project-folder.js
@@ -1,10 +1,27 @@
 'use strict'
 
+// function CreateProjectFolder() {
 const fs = require('fs');
-const projectFolder = process.argv[2];
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const question = 'Nome do projeto:\n'
+let response = ''
+
 const projectFolderCallback = function (err) {
   if (err) return console.log(err)
-  console.log('Projeto criado')
+  console.log(response)
 }
 
-fs.mkdir(projectFolder, foldersCallback)
+rl.question(question, (answer) => {
+  response = `Projeto ${answer} criado`
+
+  fs.mkdir(answer, projectFolderCallback)
+})
+// }
+
+
+// module.exports = CreateProjectFolder;
