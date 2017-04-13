46e2ef5f6f9311ac77c0f83048f8e0862ff09ac2
Evellyn <evellynlrl@hotmail.com>
2017-04-13 00:08:52 -0300

    Criando pastas JS corretamente (utilizando o módulo 'mkdirp')

--- files/create-folders-js.js
+++ files/create-folders-js.js
@@ -1,45 +1,31 @@
 'use strict'
 
 const fs = require('fs'),
+  path = require('path'),
+  mkdirp = require('mkdirp'),
+  getDirName = require('path').dirname,
   rl = require('./readline'),
-  paths = require('./global-paths'),
-  path = require('path')
+  paths = require('./global-paths')
 
 let folders;
 let response;
 const question = 'Nome das pastas (separados por vírgulo):\n';
 
 function CreateFoldersJS() {
-  const createFilesJSCallback = function (err) {
-    if (err) return console.log(err)
-    console.log(response.green)
-  }
-
   rl.question(question, (answer) => {
-    response = `Arquivos "${answer}" criado(s)`
+    response = `Arquivos "${answer}" criado(s)`;
 
     folders = answer.split(',')
     folders.forEach(folder => {
-      if (folder === '') return
-      let directory = path.normalize(__dirname + paths.JS + folder)
+      if (folder === '') return;
+      const directory = path.normalize(__dirname + paths.JS + folder);
 
-      console.log(directory.green)
-
-      fs.stat(directory, (err) => {
-        if (err && err.code === 'ENOENT') {
-          console.log('esse erro mesmo')
-          
-          fs.mkdir(directory, (err) => {
-            if (err) return console.log(err)
-            console.log('Pastas js criadas')
-          })
-        } else {
-          console.log('ERR: ', err)
-        }
-      })
-    })
-
-  })
+      mkdirp(directory, (err) => {
+        if (err) return console.log(`ERRO mkdir: "${err}"`);
+        console.log(`Pasta criada: "${directory.green}"`);
+      });
+    });
+  });
 }
 
 module.exports = CreateFoldersJS;
