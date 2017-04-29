2d237698f8e6550a016e1adbb58a4623c2de065a
Evellyn <evellynlrl@hotmail.com>
2017-04-28 22:53:22 -0300

    Executando o comando 'npm install' no projeto criado

--- files/move-package.js
+++ files/move-package.js
@@ -3,13 +3,16 @@
 const fs = require('fs')
 const rl = require('./readline')
 const paths = require('./global-paths')
+const RunNpm = require('./run-npm-install')
 
 function MovePackageJSON() {
   const fileName = 'package.json'
   const filePath = paths.projectRoot + '/' + fileName
-  
+
   console.log(`Copiando arquivo ${fileName}`.green)
   fs.createReadStream('package-to-copy.json').pipe(fs.createWriteStream(filePath))
+
+  RunNpm()
 }
 
 module.exports = MovePackageJSON
--- files/run-npm-install.js
+++ files/run-npm-install.js
@@ -1,10 +1,17 @@
 'use strict'
 
 const exec = require('child_process').exec
+const paths = require('./global-paths')
 
-exec('npm install', (err, stdout, stderr) => {
-  if (err) return console.log(err)
+function RunNpm() {
+  exec('npm install', {
+    cwd: paths.projectRoot
+  }, (err, stdout, stderr) => {
+    if (err) return console.log(err)
 
-  console.log(`stdout: ${stdout}`)
-  console.log(`stderr: ${stderr}`)
-})
+    console.log(`stdout: ${stdout}`)
+    console.log(`stderr: ${stderr}`)
+  })
+}
+
+module.exports = RunNpm
