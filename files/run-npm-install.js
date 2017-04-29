1b7a63162b641a3b6d120b22830fdd1ae135975e
Evellyn <evellynlrl@hotmail.com>
2017-04-29 00:13:16 -0300

    Executando npm install e fechando terminal

--- files/run-npm-install.js
+++ files/run-npm-install.js
@@ -1,17 +1,26 @@
 'use strict'
 
-const exec = require('child_process').exec
+const rl = require('./readline')
+const childProcess = require('child_process')
 const paths = require('./global-paths')
 
 function RunNpm() {
-  exec('npm install', {
+  console.log('Executando NPM Install...'.magenta)
+
+  let exec = childProcess.exec('npm install', {
     cwd: paths.projectRoot
   }, (err, stdout, stderr) => {
     if (err) return console.log(err)
 
-    console.log(`stdout: ${stdout}`)
     console.log(`stderr: ${stderr}`)
+    console.log(`stdout: ${stdout} \n NPM INSTALL executado com sucesso`.green)
+
+    rl.close()
   })
+  
+//  exec.on('exit', (code) => {
+//    console.log('Child process exited with exit code ', code);
+//  });
 }
 
 module.exports = RunNpm
