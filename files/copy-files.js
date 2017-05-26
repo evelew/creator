6e05f28ad56f3ad4412fe907a7c084e7d8eea92b
Evellyn Lima <evellynlrl@hotmail.com>
2017-05-26 09:12:26 -0300

    Parte do código que criava pasta 'copies' para teste foi removida

--- files/copy-files.js
+++ files/copy-files.js
@@ -3,18 +3,12 @@
 const fs = require('fs')
 const rl = require('./readline')
 const path = require('path')
-const mkdirp = require('mkdirp')
 
 const paths = require('./global-paths')
 const dir = path.normalize(__dirname + '/../copy/')
 const RunNpm = require('./run-npm-install')
 
 function CopyFiles() {
-  mkdirp(path.normalize(__dirname + '/../copies/'), (err) => {
-    if (err) return console.log(`ERRO mkdir: "${err}"`)
-    console.log('Arquivos copiados com sucesso')
-  })
-
   fs.readdir(dir, (err, files) => {
     files.forEach(file => {
       const filePath = paths.projectRoot + '/' + file
